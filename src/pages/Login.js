783aa3be7f3131b7df5bb74d75734ff100a62fbb
Jeevendra Singh <jeesingh@microsoft.com>
2023-06-14 21:13:47 +0530

    signup box

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,29 +1,48 @@
 import {useState} from 'react'
+import { Dropdown, DropdownButton } from 'react-bootstrap'
 
 function Login(){
 
     const [userId, setUserId] = useState("")
     const [password, setPassword] = useState("")
+    const [userName, setUserName] = useState("")
+    const [userEmail, setUserEmail] = useState("")
+    const [userType, setUserType] = useState("CUSTOMER")
     const [showSignUp, setShowSignUp] = useState(false)
+    const [message, setMessage] = useState("")
 
     const loginFn = (e) => {
         e.preventDefault()
         const data = {
             userId: userId,
-            password: password
+            password: password,
         }
 
-        console.log("Data is " + JSON.stringify(data))
+        console.log("Login data is " + JSON.stringify(data))
+    }
+
+    const signUpFn = (e) => {
+        e.preventDefault()
+        const data = {
+            userId: userId,
+            password: password,
+            userName: userName,
+            userEmail: userEmail,
+            userType: userType
+        }
+
+        console.log("Signup data is " + JSON.stringify(data))
     }
 
     return (
+        
         <div>
             <div className="bg-info d-flex justify-content-center align-items-center vh-100">
                 <div className="card card-signin m-5 p-5 shadow-lg rounded-4">
                     <div className="row m-2">
                         <div>
                             <h4 className="text-center">{showSignUp ? "Sign Up" : "Login"}</h4>
-                            <form onSubmit={loginFn}>
+                            <form onSubmit={showSignUp ? signUpFn : loginFn}>
                                 <div className="input-group m-1">
                                     <input 
                                     type="text"
@@ -47,19 +66,65 @@ function Login(){
                                     required
                                     />
                                 </div>
-                                <div className="input-group my-2">
+                                {
+                                    showSignUp && (
+                                    <>
+                                        <div className='input-group m-1'>
+                                            <input
+                                                type='text'
+                                                className='form-control'
+                                                placeholder='Email'
+                                                id='email'
+                                                value={userEmail}
+                                                onChange={(e) => setUserEmail(e.target.value)} 
+                                                required
+                                                />
+                                        </div>
+                                        <div className='input-group m-1'>
+                                            <input 
+                                            type='text'
+                                            className='form-control'
+                                            placeholder='User Name'
+                                            value={userName}
+                                            onChange={(e) => setUserName(e.target.value)}
+                                            required
+                                            />
+                                        </div>
+                                        <div className='row input-group'>
+
+                                            <div className='col py-2'>
+                                                <span className='mx-1 my-1'>User Type</span>
+                                            </div>
+
+                                            <div className='col'>
+                                            <DropdownButton align='end' title={userType} id='userType' onSelect={(e) => setUserType(e.target.value)} variant='light'>
+                                                <Dropdown.Item eventKey="CUSTOMER">CUSTOMER</Dropdown.Item>
+                                                <Dropdown.Item eventKey="ENGINEER">ENGINEER</Dropdown.Item>
+                                            </DropdownButton>
+                                        </div>
+                                        </div>
+                                    </>
+                                    )
+                                }
+                                <div className='input-group m-1'>
                                     <input 
                                     type="submit"
                                     className="form-control btn btn-primary"
-                                    value={"Login"}
+                                    value={showSignUp ? "Sign Up" : "Login"}     
                                     />
+                                </div>
+                                <div onClick={(e) => setShowSignUp(!showSignUp)}>
+                                    {showSignUp? "Already have an account? Login": "Don't have an account? Signup"}
+                                </div>
+                                <div className='text-success text-center'>
+                                    {message}
                                 </div>
                             </form>
                         </div>
-                    </div>
+                    </div>  
                 </div>
             </div>
-        </div>
+        </div> 
     )
 }
 
